a9d86ce350b22e3961863bb9d45004521c30fc69
selimRU <selim.soha92@gmail.com>
2023-07-30 11:53:08 +0600

    problem solved

---
+++ problemSolved.js
@@ -0,0 +1,47 @@
+var me = 85;
+var tom = 66;
+var jane = 95;
+var prter = 40;
+var john = 56;
+
+// me
+if (me >= 80) {
+    console.log('my result is A');
+
+}
+else if (me <= 60) {
+    console.log('B');
+}
+else if (me <= 50) {
+    console.log('C');
+}
+else if (me <= 40) {
+    console.log('D');
+}
+else if (me <= 39) {
+    console.log('F');
+}
+else {
+    console.log('absent');
+}
+
+// tom
+if (tom >= 80) {
+    console.log('tom is  A');
+
+}
+else if (tom >= 60) {
+    console.log('tom is B');
+}
+else if (tom <= 50) {
+    console.log('tom is C');
+}
+else if (tom <= 40) {
+    console.log('tom is D');
+}
+else if (tom <= 39) {
+    console.log('tom is F');
+}
+else {
+    console.log('tom is absent');
+}
